7af9d4303367a30b680ec7313196e6c1cb102e04
Noah Evans <noah.evans@gmail.com>
2015-07-21 18:53:56 -0400

    emergency commit due to file corruption on host

--- analytics-kitten-tcasm-enclave.c
+++ analytics-kitten-tcasm-enclave.c
@@ -17,34 +17,54 @@ struct usda {
 	double d[3];
 	void *flux;
 	void *v;
+	int niter;
+	int mine;
 };
 
 sem_t *lock;
 char lock_name[10] = "", filename[20] = "";
 struct usda *cte;
 void *data;
-int fd;
+int myiter;
 
 int setup_(int *nx, int *ny, int *nz, int *ng, double *dx, double *dy, 
-		double *dz, int *iproc, char *segment) {
+		double *dz, int *iproc, char *fseg, int len) {
 	xemem_segid_t segid;
 	xemem_apid_t apid;
 	struct xemem_addr addr;
 	void *v;
+	int i;
+	char *segment;
+
+	printf("fixing fseg %s with len %d\n",fseg, len);
+	fseg[23]=0;
+	segment = fseg;
+	printf("looking up segid %s len %d\n", segment, strlen(segment));
+	// add some sort of synchronization here? There's a race between creation and use.
+		
 	segid = xemem_lookup_segid(segment);
+	i = 1000;
+	while(segid < 0 && i--){
+		sleep(1);
+		segid = xemem_lookup_segid(segment);
+	}
+
 	if(segid < 0) {
 		printf("failed to lookup segment\n");
 		exit(-1);
 	}
+
+	printf("xemem getting\n");
 	apid = xemem_get(segid, XEMEM_RDWR);
 	if(apid < 0) {
 		printf("failed to lookup apid for segment %ld\n", segid);
 		exit(-1);
 	}
+
 	addr.apid = apid;
 	addr.offset = 0;
 	// round up?
-
+	printf("xemem attaching\n");
 	cte = (struct usda *)xemem_attach(addr, sizeof(struct usda), NULL);
 	if(cte == NULL) {
 		printf("could not attach to addr apid %lx offset %ld\n", 
@@ -53,12 +73,12 @@ int setup_(int *nx, int *ny, int *nz, int *ng, double *dx, double *dy,
 	}
 // I want to check on how signalling versus polling works, we're going to poll
 // now.	
-	while(cte->time_loop == 0) {
+	//while(cte->time_loop == 0) {
 		// how do you find this?
 		// what I'm doing is actually a signal wait.
 		// we're reopening the shared memory here.
 		// that is not necessary.
-	}
+	//}
 
 	*nx = cte->n[0];
 	*ny = cte->n[1];
@@ -69,6 +89,9 @@ int setup_(int *nx, int *ny, int *nz, int *ng, double *dx, double *dy,
 	*dy = cte->d[1];
 	*dz = cte->d[2];
 
+	myiter = cte->niter;
+	cte->mine = 1;
+	printf("done setting up\n");
 	// need to set up the fluxes
 	return 1;
 }
@@ -77,9 +100,19 @@ int allocate_(void **flux, void **v, int *cy, int *fin) {
 	size_t size1 = 8, size2 = 8*cte->n[3];
 	int i;
 
-	// will I need any locking here?
-	// I already have the cte, is it being updated?
-
+	printf("allocating\n");
+	// poll for a new update
+	while(myiter == cte->niter && !cte->finalized){
+		printf("waiting for iter myiter %d cte->niter %d cte->mine %d \n", myiter, cte->niter, cte->mine);
+	}
+		printf("got myiter %d cte->niter %d cte->mine %d cte->finalized %d\n", myiter, cte->niter, cte->mine, cte->finalized);
+	// XXX: this is a botch fix it later
+	if(cte->finalized)
+		exit(1);
+	printf("it's mine again\n");
+	cte->mine = 1;
+	printf("got an update\n");
+	myiter = cte->niter;
 	for(i=0; i < 4; i++)
 		size1 *= cte->n[i];
 	// so you are allocating two things here.
@@ -93,6 +126,7 @@ int allocate_(void **flux, void **v, int *cy, int *fin) {
 	*cy = cte->time_loop;
 	*fin = cte->finalized;
 
+	printf("finished allocating\n");
 	return 1;
 }
 
@@ -103,5 +137,6 @@ int deallocate_() {
 
 int close_(){
 	printf("closing\n");
+	for(;;);
 	return 1;
 }
--- analyze.f90
+++ analyze.f90
@@ -24,53 +24,73 @@ PROGRAM analyze
 
   REAL(r_knd) :: dx, dy, dz  
 
-  CHARACTER(len=32) :: arg
+  CHARACTER(len=64) :: segment
 
   type(c_ptr) :: cptr1, cptr2
   ! do we need mpi here?
   !CALL MPI_INIT(ierr)
 
   root=0;
-
-  CALL getarg(1, arg)
-  read(arg,*) nproc
+  nproc=0;
+  WRITE(*,*) 'starting analysis'
+  IF(IARGC() /= 1) THEN
+    WRITE(*,*) 'usage: analyze'
+    CALL EXIT(-1)
+  END IF
+  CALL getarg(1, segment)
+  !read(arg,*) nproc
 
   !CALL MPI_COMM_SIZE ( MPI_COMM_WORLD, nproc, ierr )
   !CALL MPI_COMM_RANK ( MPI_COMM_WORLD, iproc, ierr )
 
-  CALL setup(nx, ny, nz, ng, dz, dy, dz, iproc)
+  WRITE(*,*) 'setting up'
+  CALL setup(nx, ny, nz, ng, dz, dy, dz, iproc, segment, LEN(segment))
 
+  WRITE(*,*) 'allocating basics'
   ALLOCATE( tmp(nx,ny,nz,ng), STAT=ierr)
   ALLOCATE( pop(ng), STAT=ierr)
-  ALLOCATE( res(ng), STAT=ierr)  
+  ALLOCATE( res(ng), STAT=ierr)
 
+  WRITE(*,*) 'doing C allocation'
   CALL allocate(cptr1, cptr2, cy, fin)
 
+  WRITE(*,*) 'doing loop'
   loop: DO WHILE ( fin == 0)
      CALL C_F_POINTER(cptr1, flux, [nx,ny,nz,ng])
      CALL C_F_POINTER(cptr2, v, [ng])
 
+  WRITE(*,*) 'iteration'
      pop=zero
-     tmp = flux*dx*dy*dz
-
+     WRITE(*, *) 'popped'
+     !tmp = flux*dx*dy*dz
+    WRITE(*, *) 'tmp'
      DO g=1, ng
+        WRITE(*, *) 'popping'
         pop(g) = SUM(tmp(:,:,:,g))
      END DO
 
      !CALL MPI_REDUCE(pop, res, ng, MPI_REAL8, MPI_SUM, root, MPI_COMM_WORLD, ierr)
 
-     pop = pop/v
+!     pop = pop/v
 
      IF(iproc == root) THEN
         WRITE ( *,* ) cy,":"
         DO g=1, ng
            WRITE ( *,* ) pop(g)
         END DO
-     END IF
+    END IF
+    
+  WRITE(*,*) 'deallocate'
      CALL deallocate
+     
+  WRITE(*,*) 'new allocation'
      CALL allocate(cptr1, cptr2, cy, fin)
   END DO loop
+  
+  WRITE(*,*) 'deallocating'
   CALL deallocate
+
+  WRITE(*,*) 'closing'
   CALL close
   !CALL MPI_FINALIZE ( ierr )
 END PROGRAM analyze
